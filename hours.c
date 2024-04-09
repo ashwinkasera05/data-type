88947b1797e89d4d513170fd649d7579872fc397
ashwinkasera05 <ashwintamrakar09>
2024-04-09 23:29:16 +0530

    new file added

--- hours.c
+++ hours.c
@@ -5,10 +5,10 @@ int main()
     printf("enter a second:");
     scanf("%d",&second);
     hours=second/3600;
-    mints=(second%3600)/60;
-    second=(second)%60%3600;
-    printf("HOURS =%d\n",second);
-    printf("Mints =%d\n",second);
+    mints=(second-(3600*hours))/60;
+    second=(second-(3600*hours)-(mints*60));
+    printf("HOURS =%d\n",hours);
+    printf("Mints =%d\n",mints);
     printf("Second =%d\n",second);
 
     return 0;
--- reverse.c
+++ reverse.c
@@ -6,7 +6,7 @@ void main()
     scanf("%d",&n);
     a=n/100;
     b=(n%100)/10;
-    c=(n%10)/100;
-    rev=c*100+b*10;
+    c=(n%10);
+    rev=c*100+b*10+a;
     printf("reverse the value %d",rev);
 }
